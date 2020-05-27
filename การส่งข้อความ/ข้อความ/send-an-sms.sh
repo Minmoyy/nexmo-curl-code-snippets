a28460b065c469e0bd12819e0613ae99a9b29c40
Minmoyy <66036626+Minmoyy@users.noreply.github.com>
2020-05-28 05:41:13 +0700

    Update and rename messaging/sms/send-an-sms.sh to การส่งข้อความ/ข้อความ/send-an-sms.sh

--- การส่งข้อความ/ข้อความ/send-an-sms.sh
+++ การส่งข้อความ/ข้อความ/send-an-sms.sh
@@ -1,5 +1,3 @@
-#!/usr/bin/env bash
-source "../../config.sh"
 
 curl -X "POST" "https://rest.nexmo.com/sms/json" \
   -d "from=$NEXMO_BRAND_NAME" \
@@ -7,3 +5,4 @@ curl -X "POST" "https://rest.nexmo.com/sms/json" \
   -d "to=$TO_NUMBER" \
   -d "api_key=$NEXMO_API_KEY" \
   -d "api_secret=$NEXMO_API_SECRET"
+สำเนา
